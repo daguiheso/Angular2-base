d3f4e9f8e136518c41004ed36bc75515360834b5
Daniel hernandez <danielhernandez16@hotmail.com>
2016-12-11 21:06:44 -0500

    Base structure proyect Angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+// Decorador con objeto
+
+/*
+ * El decorador de componente me permite extender atributos de la clase que estoy generando
+ * es decir el componente a traves del objeto va a extender los atributos que existen en la
+ * clase AppComponent. Va a generar un selector y un template ligado directamente a esta clase
+ */
+@Component({
+  selector: 'my-app', // tag de angular 1.x para iniciar el component
+  template: `<h1>Hello {{name}}</h1>`,
+})
+
+// exportando clase con el nombre que queramos
+export class AppComponent  { name = 'Angular'; }
---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+// permite importacion de modulos y componentes
+import { NgModule } from '@angular/core';
+// permite que codigo se ejecute en cualquier navegador
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+
+// Decorador con objeto
+@NgModule({
+  // importando modulos que se necesitaran en la plataforma
+  imports: [BrowserModule],
+  // declaracion de todos los component generados
+  declarations: [AppComponent],
+  // para indicar que componente va a ser el principal en el proyecto
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
